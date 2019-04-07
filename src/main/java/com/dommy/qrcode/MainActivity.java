2bafb1426b1c4000eee251f625a1d4c1a76bdd26
27269 <272698169@qq.com>
2019-04-07 21:26:35 +0800

    creat

---
+++ src/main/java/com/dommy/qrcode/MainActivity.java
@@ -0,0 +1,139 @@
+package com.dommy.qrcode;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dommy.qrcode.util.Constant;
+import com.google.zxing.activity.CaptureActivity;
+import com.google.zxing.encoding.EncodingHandler;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnQrCode,btn_gen; // 扫码
+    TextView tvResult; // 结果
+    private EditText etCode;//生成二维码的文本
+    private ImageView imgQRCode;//显示生成的二维码图像
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        btnQrCode = (Button) findViewById(R.id.btn_qrcode);
+        btnQrCode.setOnClickListener(this);
+        btn_gen=(Button)findViewById(R.id.btn_gen);
+        btn_gen.setOnClickListener(genOnClick);
+        etCode=(EditText) findViewById(R.id.et_code);
+        imgQRCode=(ImageView)findViewById(R.id.img_qrcode);
+        tvResult = (TextView) findViewById(R.id.txt_result);
+    }
+    /**
+     * 生成二维码
+     */
+    private void startGen() {
+        String content = etCode.getText().toString();
+        if ("".equalsIgnoreCase(content)) {
+            Toast.makeText(this, "内容不能为空", Toast.LENGTH_SHORT).show();
+        } else {
+            //调用工具类EncodingUtils生成二维码图片
+            Bitmap qrCode = EncodingHandler.createQRCode(content, 600, 600, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+            imgQRCode.setImageBitmap(qrCode);
+        }
+    }
+    private View.OnClickListener genOnClick=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startGen();
+        }
+    };
+    // 开始扫码
+    private void startQrCode() {
+        // 申请相机权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // 申请权限
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, Constant.REQ_PERM_CAMERA);
+            return;
+        }
+        // 申请文件读写权限（部分朋友遇到相册选图需要读写权限的情况，这里一并写一下）
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // 申请权限
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_EXTERNAL_STORAGE);
+            return;
+        }
+        // 二维码扫码
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, Constant.REQ_QR_CODE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_qrcode:
+                startQrCode();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //扫描结果回调
+        if (requestCode == Constant.REQ_QR_CODE && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString(Constant.INTENT_EXTRA_KEY_QR_SCAN);
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.VIEW");
+            Uri url = Uri.parse(scanResult);
+            intent.setData(url);
+            startActivity(intent);
+//            //将扫描出的信息显示出来
+//            tvResult.setText(scanResult);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case Constant.REQ_PERM_CAMERA:
+                // 摄像头权限申请
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获得授权
+                    startQrCode();
+                } else {
+                    // 被禁止授权
+                    Toast.makeText(MainActivity.this, "请至权限中心打开本应用的相机访问权限", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case Constant.REQ_PERM_EXTERNAL_STORAGE:
+                // 文件读写权限申请
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获得授权
+                    startQrCode();
+                } else {
+                    // 被禁止授权
+                    Toast.makeText(MainActivity.this, "请至权限中心打开本应用的文件读写权限", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
+
+
+}
